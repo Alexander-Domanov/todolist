ed1cb6a4ee40cbf97772c851ea889ae369a933f7
Alexander.D <74902666+lonegdd@users.noreply.github.com>
2023-03-22 23:44:33 +0300

    Updates

---
+++ precache-manifest.35e7fb0052ff2e1d7ef97062888f3adf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "57b1afc306b95fb55d0b73edc2201084",
+    "url": "/todolist/index.html"
+  },
+  {
+    "revision": "bf1619f2a55851420da1",
+    "url": "/todolist/static/css/main.155097fa.chunk.css"
+  },
+  {
+    "revision": "eedc512af3c91e6cc62b",
+    "url": "/todolist/static/js/2.317bd65b.chunk.js"
+  },
+  {
+    "revision": "e2896a34bc173bbd7b6d83453e91d5e1",
+    "url": "/todolist/static/js/2.317bd65b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bf1619f2a55851420da1",
+    "url": "/todolist/static/js/main.35c7d197.chunk.js"
+  },
+  {
+    "revision": "471e54e937a3aef18e16",
+    "url": "/todolist/static/js/runtime-main.283d8678.js"
+  }
+]);
